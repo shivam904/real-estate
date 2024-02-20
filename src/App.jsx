2091c632ea36f363984e621d067a8bbd495ca379
shivam <negishivam904@gmail.com>
2024-02-20 22:48:47 +0530

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUP";
+import Header from "./components/Header";
 export default function App() {
-  return (
-    <div className='text'>
-      
-    </div>
-  )
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element = {<Home/>}/>
+    <Route path="/sign-in" element = {<Signin/>}/>
+    <Route path="/sign-up" element = {<SignUp/>}/>
+    <Route path="/profile" element = {<Profile/>}/>
+    
+    </Routes>
+    </BrowserRouter>
 }
